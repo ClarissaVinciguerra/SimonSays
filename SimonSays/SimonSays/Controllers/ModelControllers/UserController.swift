df978bf087e0f32359d13699359aca75f5c89bfc
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2020-10-14 13:34:35 -0700

    Outlines user controllers

---
+++ SimonSays/SimonSays/Controllers/ModelControllers/UserController.swift
@@ -0,0 +1,20 @@
+//
+//  UserController.swift
+//  SimonSays
+//
+//  Created by Clarissa Vinciguerra on 10/14/20.
+//
+
+import Foundation
+
+class UserController {
+    // MARK: - Properties
+    static let shared = UserController()
+    //var users: [User] = []
+    
+    // MARK: - CRUD
+    func addUser() {
+        
+    }
+    
+}
