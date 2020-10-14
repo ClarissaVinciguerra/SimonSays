381f20024cb05494cf0899950a68fb17a67fa794
Dev <devenlukeday@gmail.com>
2020-10-14 17:24:17 -0600

    Creat ColorConstants file, User Model, & Move file

--- SimonSays/SimonSays/Models/User.swift
+++ SimonSays/SimonSays/Models/User.swift
@@ -2,7 +2,17 @@
 //  User.swift
 //  SimonSays
 //
-//  Created by Clarissa Vinciguerra on 10/14/20.
+//  Created by Deven Day on 10/14/20.
 //
 
 import Foundation
+
+class User {
+    var name: String
+    var score: Int
+    
+    init(name: String, score: Int) {
+        self.name = name
+        self.score = score
+    }
+}//END OF CLASS
