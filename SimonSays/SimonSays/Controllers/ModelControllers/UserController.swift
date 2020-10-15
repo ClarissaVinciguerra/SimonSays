458318394ca05e6d7c3877c1ecebdb23cad13018
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2020-10-15 13:37:25 -0700

    Changes add user function to return a single user

--- SimonSays/SimonSays/Controllers/ModelControllers/UserController.swift
+++ SimonSays/SimonSays/Controllers/ModelControllers/UserController.swift
@@ -13,10 +13,10 @@ class UserController {
     var users: [User] = []
     
     // MARK: - CRUD
-    func addUser(name: String, score: Int) -> [User] {
+    func addUser(name: String, score: Int) -> User {
         let newUser = User(name: name, score: score)
         UserController.shared.users.append(newUser)
-        let sortedUsers = UserController.shared.users.sorted(by: { $0.score > $1.score })
-        return sortedUsers
+        UserController.shared.users.sorted(by: { $0.score > $1.score })
+        return newUser
     }
 }
