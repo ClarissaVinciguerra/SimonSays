3b4f88334bd80b4693bed88f3512e57b82e1a454
LAURA JELENICH <laurajelenich@LAURAs-MBP-2.lan1>
2020-10-15 14:30:15 -0400

    Adds outlets to scoreboard view controller + gameplay view controller

--- SimonSays/SimonSays/View Controllers/GamePlayViewController.swift
+++ SimonSays/SimonSays/View Controllers/GamePlayViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class GamePlayViewController: UIViewController {
-
+    @IBOutlet weak var redButton: UIView!
+    @IBOutlet weak var greenButton: UIView!
+    @IBOutlet weak var blueButton: UIView!
+    @IBOutlet weak var yellowButton: UIView!
+    @IBOutlet weak var gameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    @IBOutlet weak var redButtonPressed: UIView!
+    @IBOutlet weak var greenButtonPressed: UIView!
+    @IBOutlet weak var blueButtonPressed: UIView!
+    @IBOutlet weak var yellowButtonPressed: UIView!
+    
+    
 }
